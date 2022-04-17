73924b4e82153b67f241cec0e9ca238c22e04474
Berkcan Uçan <hello@alca.dev>
2022-04-17 17:34:35 +0300

    feat: added helpers

---
+++ src/utils/helpers.ts
@@ -0,0 +1,28 @@
+import { mkdir } from 'node:fs/promises';
+
+import { printMessage } from '../utils/cli';
+
+/**
+ * Ensures directory exists at given path.
+ * @param {string} path
+ * @returns {Promise}
+ */
+export const ensureDirectory = (path: string) => {
+  try {
+    return mkdir(path, { recursive: true });
+  } catch (error) {
+    printMessage(error, 'error', `Cannot create directory ${path}`);
+  }
+};
+
+/**
+ * Sets .html mime type if not exist.
+ * @param {string} route
+ * @returns {string} Full file name
+ */
+export const setDefaultMIMEType = (route: string) => {
+  const fileName = route === '/' ? '/index' : route;
+  const withMIMEType = !!fileName.match(/(.htm$|.html$|.php$)/i);
+
+  return withMIMEType ? fileName : `${fileName}.html`;
+};
